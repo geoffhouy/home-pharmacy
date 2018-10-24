40f21c2a7536470855764f8077070c6f41446b32
Geoffrey Houy <geoffhouy@gmail.com>
2018-10-24 18:37:21 -0400

    Remove unused import

--- app/src/main/java/edu/wvu/statler/lcsee/cs450/group4/homepharmacy/db/entities/Schedule.java
+++ app/src/main/java/edu/wvu/statler/lcsee/cs450/group4/homepharmacy/db/entities/Schedule.java
@@ -4,7 +4,6 @@ import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.PrimaryKey;
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
