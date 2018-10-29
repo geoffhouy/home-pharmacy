a960a815ab98b9b159d5ce816de623e751696e0c
Geoffrey Houy <geoffhouy@gmail.com>
2018-10-29 16:34:38 -0400

    Add testCallback to clear and re-populate database onOpen

--- app/src/main/java/edu/wvu/statler/lcsee/cs450/group4/homepharmacy/db/HomePharmacyDatabase.java
+++ app/src/main/java/edu/wvu/statler/lcsee/cs450/group4/homepharmacy/db/HomePharmacyDatabase.java
@@ -1,9 +1,12 @@
 package edu.wvu.statler.lcsee.cs450.group4.homepharmacy.db;
 
+import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
 
 import edu.wvu.statler.lcsee.cs450.group4.homepharmacy.db.dao.MedicationDao;
 import edu.wvu.statler.lcsee.cs450.group4.homepharmacy.db.dao.ScheduleDao;
@@ -15,7 +18,7 @@ import edu.wvu.statler.lcsee.cs450.group4.homepharmacy.db.entity.User;
 /**
  * The Room database for Home Pharmacy.
  */
-@Database(entities = {User.class, Medication.class, Schedule.class}, version = 1)
+@Database(entities = {User.class, Medication.class, Schedule.class}, exportSchema = false, version = 1)
 public abstract class HomePharmacyDatabase extends RoomDatabase {
 
     /**
@@ -50,11 +53,39 @@ public abstract class HomePharmacyDatabase extends RoomDatabase {
                 if (INSTANCE == null) {
                     INSTANCE = Room
                             .databaseBuilder(context.getApplicationContext(), HomePharmacyDatabase.class, "home-pharmacy")
+                            .addCallback(testCallback)
                             .build();
                 }
             }
         }
         return INSTANCE;
+    }
+
+    private static RoomDatabase.Callback testCallback = new RoomDatabase.Callback() {
+
+        @Override
+        public void onOpen(@NonNull SupportSQLiteDatabase db) {
+            super.onOpen(db);
+            new PopulateAsyncTask(INSTANCE).execute();
+        }
+
+    };
+
+    private static class PopulateAsyncTask extends AsyncTask<Void, Void, Void> {
+
+        private final UserDao userDao;
+
+        PopulateAsyncTask(HomePharmacyDatabase db) {
+            userDao = db.userDao();
+        }
+
+        @Override
+        protected Void doInBackground(final Void... params) {
+            userDao.deleteAll();
+            userDao.insert(new User("John Doe", "1234"));
+            userDao.insert(new User("Jane Doe", "4321"));
+            return null;
+        }
     }
 
 }
