a8f8ad4be3a9dda8202eed668e7bb08694535470
Geoffrey Houy <geoffhouy@gmail.com>
2018-10-29 16:38:41 -0400

    Add NewUserActivity functionality

--- app/src/main/java/edu/wvu/statler/lcsee/cs450/group4/homepharmacy/ui/MainActivity.java
+++ app/src/main/java/edu/wvu/statler/lcsee/cs450/group4/homepharmacy/ui/MainActivity.java
@@ -1,17 +1,33 @@
 package edu.wvu.statler.lcsee.cs450.group4.homepharmacy.ui;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.List;
 
 import edu.wvu.statler.lcsee.cs450.group4.homepharmacy.R;
+import edu.wvu.statler.lcsee.cs450.group4.homepharmacy.db.entity.User;
+import edu.wvu.statler.lcsee.cs450.group4.homepharmacy.ui.adapter.UserListAdapter;
+import edu.wvu.statler.lcsee.cs450.group4.homepharmacy.viewmodel.UserViewModel;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final int NEW_USER_ACTIVITY_REQUEST_CODE = 1;
+
+    private UserViewModel userViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,14 +35,41 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        //
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+        final UserListAdapter userListAdapter = new UserListAdapter(this);
+        recyclerView.setAdapter(userListAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        userViewModel = ViewModelProviders.of(this).get(UserViewModel.class);
+
+        userViewModel.getAllUsers().observe(this, new Observer<List<User>>() {
+            @Override
+            public void onChanged(@Nullable List<User> users) {
+                userListAdapter.setUsers(users);
+            }
+        });
+
+        //
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        /*
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
+        });
+        */
+        fab.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, NewUserActivity.class);
+                startActivityForResult(intent, NEW_USER_ACTIVITY_REQUEST_CODE);
+            }
+
         });
     }
 
@@ -50,5 +93,19 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == NEW_USER_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK) {
+            User user = new User(data.getStringExtra(NewUserActivity.EXTRA_REPLY), "1234");
+            userViewModel.insert(user);
+        } else {
+            Toast.makeText(
+                    getApplicationContext(),
+                    R.string.empty_not_saved,
+                    Toast.LENGTH_LONG).show();
+        }
     }
 }
