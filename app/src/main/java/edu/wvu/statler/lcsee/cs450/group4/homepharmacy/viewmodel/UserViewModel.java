3e6d96bb58269723a21cd4d7daa8fc7fbc5c7ed7
Geoffrey Houy <geoffhouy@gmail.com>
2018-10-29 15:36:42 -0400

    Add UserViewModel to connect UserRepository and UI

---
+++ app/src/main/java/edu/wvu/statler/lcsee/cs450/group4/homepharmacy/viewmodel/UserViewModel.java
@@ -0,0 +1,40 @@
+package edu.wvu.statler.lcsee.cs450.group4.homepharmacy.viewmodel;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+
+import java.util.List;
+
+import edu.wvu.statler.lcsee.cs450.group4.homepharmacy.db.entity.User;
+import edu.wvu.statler.lcsee.cs450.group4.homepharmacy.db.repository.UserRepository;
+
+public class UserViewModel extends AndroidViewModel {
+
+    private UserRepository userRepository;
+
+    public UserViewModel(Application application) {
+        super(application);
+        userRepository = new UserRepository(application);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.getAllUsers();
+    }
+
+    public User getUserByUUID(long uuid) {
+        return userRepository.getUserByUUID(uuid);
+    }
+
+    public void insert(User user) {
+        userRepository.insert(user);
+    }
+
+    public void update(User user) {
+        userRepository.update(user);
+    }
+
+    public void delete(User user) {
+        userRepository.delete(user);
+    }
+
+}
