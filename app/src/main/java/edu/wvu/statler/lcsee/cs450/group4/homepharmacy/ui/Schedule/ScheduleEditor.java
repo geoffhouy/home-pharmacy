d5e421a1961474cf63cbc3cd4e9898856b55deaf
alex123abc15 <alexwilson30@gmail.com>
2018-11-07 13:36:48 -0500

    Added popup to get the date in ScheduleEditor

--- app/src/main/java/edu/wvu/statler/lcsee/cs450/group4/homepharmacy/ui/Schedule/ScheduleEditor.java
+++ app/src/main/java/edu/wvu/statler/lcsee/cs450/group4/homepharmacy/ui/Schedule/ScheduleEditor.java
@@ -1,11 +1,22 @@
 package edu.wvu.statler.lcsee.cs450.group4.homepharmacy.ui.Schedule;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputEditText;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 import edu.wvu.statler.lcsee.cs450.group4.homepharmacy.R;
 
@@ -34,5 +45,28 @@ public class ScheduleEditor extends AppCompatActivity {
                 finish();
             }
         });
+
+        final Calendar myCalendar = Calendar.getInstance();
+        final TextInputEditText dateEdit = (TextInputEditText) findViewById(R.id.ScheduleEditorDateInput);
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                myCalendar.set(Calendar.YEAR, i);
+                myCalendar.set(Calendar.MONTH,i1);
+                myCalendar.set(Calendar.DAY_OF_MONTH,i2);
+                String myFormat = "MM/dd/yy";
+                SimpleDateFormat sdf = new SimpleDateFormat(myFormat,Locale.US);
+                dateEdit.setText(sdf.format(myCalendar.getTime()));
+            }
+        };
+        dateEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(ScheduleEditor.this,date,myCalendar.get(Calendar.YEAR),myCalendar.get(Calendar.MONTH),myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+        //Do something similar to above here for the time
     }
 }
