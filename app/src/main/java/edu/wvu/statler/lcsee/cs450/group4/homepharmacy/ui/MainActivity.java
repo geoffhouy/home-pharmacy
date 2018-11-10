e56124417177936d92df308a9aab303742723913
Geoffrey Houy <geoffhouy@gmail.com>
2018-11-09 21:43:42 -0500

    Remove unused code

--- app/src/main/java/edu/wvu/statler/lcsee/cs450/group4/homepharmacy/ui/MainActivity.java
+++ app/src/main/java/edu/wvu/statler/lcsee/cs450/group4/homepharmacy/ui/MainActivity.java
@@ -7,19 +7,13 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
 
 import edu.wvu.statler.lcsee.cs450.group4.homepharmacy.R;
 import edu.wvu.statler.lcsee.cs450.group4.homepharmacy.ui.pill.PillMenu;
 import edu.wvu.statler.lcsee.cs450.group4.homepharmacy.ui.schedule.ScheduleMenu;
 import edu.wvu.statler.lcsee.cs450.group4.homepharmacy.ui.user.UserMenu;
-import edu.wvu.statler.lcsee.cs450.group4.homepharmacy.viewmodel.UserViewModel;
 
 public class MainActivity extends AppCompatActivity {
-
-    public static final int NEW_USER_ACTIVITY_REQUEST_CODE = 1;
-
-    private UserViewModel userViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,43 +44,6 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        //
-        /*
-        RecyclerView recyclerView = findViewById(R.id.recyclerview);
-        final UserListAdapter userListAdapter = new UserListAdapter(this);
-        recyclerView.setAdapter(userListAdapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        userViewModel = ViewModelProviders.of(this).get(UserViewModel.class);
-
-        userViewModel.getAllUsers().observe(this, new Observer<List<User>>() {
-            @Override
-            public void onChanged(@Nullable List<User> users) {
-                userListAdapter.setUsers(users);
-            }
-        });
-
-        //
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);*/
-        /*
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        */
-        /*fab.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, NewUserActivity.class);
-                startActivityForResult(intent, NEW_USER_ACTIVITY_REQUEST_CODE);
-            }
-
-        });*/
     }
 
     @Override
@@ -111,16 +68,4 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == NEW_USER_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK) {
-            userViewModel.createUser(data.getStringExtra(NewUserActivity.EXTRA_REPLY), "1234");
-        } else {
-            Toast.makeText(
-                    getApplicationContext(),
-                    "User not saved because no information was entered.",
-                    Toast.LENGTH_LONG).show();
-        }
-    }
 }
